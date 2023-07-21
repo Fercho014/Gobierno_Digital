f4501b458fc17b3d32d5091321030f7853fd5c79
Fercho014 <ochoafernando014@gmail.com>
2023-07-20 23:29:29 -0600

    Prueba Laravel
    Prueba Laravel

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserCreateRequest;
+use App\Http\Requests\UserEditRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Log;
+
+class UserController extends Controller
+{
+
+    public function authenticate(Request $request)
+    {
+      $credentials = $request->only('email', 'password');
+      try {
+          if (! $token = JWTAuth::attempt($credentials)) {
+              return response()->json(['error' => 'invalid_credentials'], 400);
+          }
+      } catch (JWTException $e) {
+          return response()->json(['error' => 'could_not_create_token'], 500);
+      }
+      return response()->json(compact('token'));
+    }
+
+    public function getAuthenticatedUser()
+    {
+        try {
+          if (!$user = JWTAuth::parseToken()->authenticate()) {
+                  return response()->json(['user_not_found'], 404);
+          }
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+                return response()->json(['token_expired'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+                return response()->json(['token_invalid'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+                return response()->json(['token_absent'], $e->getStatusCode());
+        }
+        return response()->json(compact('user'));
+    }
+
+
+    public function register(Request $request)
+    {
+
+        Log::info($request);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if($validator->fails()){
+                return response()->json($validator->errors()->toJson(),400);
+        }
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user','token'),201);
+    }
+
+
+
+
+
+    public function index()
+    {
+        abort_if(Gate::denies('user_index'), 403);
+        $users = User::paginate(5);
+        return view('users.index', compact('users'));
+    }
+
+    public function create()
+    {
+        abort_if(Gate::denies('user_create'), 403);
+        $roles = Role::all()->pluck('name', 'id');
+        return view('users.create', compact('roles'));
+    }
+
+    public function store(UserCreateRequest $request)
+    {
+        // $request->validate([
+        //     'name' => 'required|min:3|max:5',
+        //     'username' => 'required',
+        //     'email' => 'required|email|unique:users',
+        //     'password' => 'required'
+        // ]);
+        $user = User::create($request->only('name', 'username', 'email')
+            + [
+                'password' => bcrypt($request->input('password')),
+            ]);
+
+        $roles = $request->input('roles', []);
+        $user->syncRoles($roles);
+        return redirect()->route('users.show', $user->id)->with('success', 'Usuario creado correctamente');
+    }
+
+    public function show(User $user)
+    {
+        abort_if(Gate::denies('user_show'), 403);
+        // $user = User::findOrFail($id);
+        // dd($user);
+        $user->load('roles');
+        return view('users.show', compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        abort_if(Gate::denies('user_edit'), 403);
+        $roles = Role::all()->pluck('name', 'id');
+        $user->load('roles');
+        return view('users.edit', compact('user', 'roles'));
+    }
+
+    public function update(UserEditRequest $request, User $user)
+    {
+        // $user=User::findOrFail($id);
+        $data = $request->only('name', 'username', 'email');
+        $password=$request->input('password');
+        if($password)
+            $data['password'] = bcrypt($password);
+        // if(trim($request->password)=='')
+        // {
+        //     $data=$request->except('password');
+        // }
+        // else{
+        //     $data=$request->all();
+        //     $data['password']=bcrypt($request->password);
+        // }
+
+        $user->update($data);
+
+        $roles = $request->input('roles', []);
+        $user->syncRoles($roles);
+        return redirect()->route('users.show', $user->id)->with('success', 'Usuario actualizado correctamente');
+    }
+
+    public function destroy(User $user)
+    {
+        abort_if(Gate::denies('user_destroy'), 403);
+
+        if (auth()->user()->id == $user->id) {
+            return redirect()->route('users.index');
+        }
+
+        $user->delete();
+        return back()->with('succes', 'Usuario eliminado correctamente');
+    }
+}
